5c51321050027e06548732b84608d833ded5a1e1
cakoch10 <cakoch10@gmail.com>
2019-07-22 16:35:13 -0400

    updated for pull request

--- src/katb/compile.ml
+++ src/katb/compile.ml
@@ -3,29 +3,28 @@ open Ast
 open Netkat_.Generic_ast
 
 
-let rec compile_pred mgr label bexpr = 
+let rec compile_pred mgr ~label bexpr = 
   match bexpr with
   | Tru -> Bdd.ctrue
   | Fls -> Bdd.cfalse
   | Test { name; value } -> Bdd.test mgr (Var.inp (label name)) value
-  | Neg b -> Bdd.neg mgr (compile_pred mgr label b)
+  | Neg b -> Bdd.neg mgr (compile_pred mgr ~label b)
   | Conj (e1, e2) -> 
-      Bdd.conj mgr (compile_pred mgr label e1) (compile_pred mgr label e2)
+      Bdd.conj mgr (compile_pred mgr ~label e1) (compile_pred mgr ~label e2)
   | Disj (e1, e2) -> 
-      Bdd.disj mgr (compile_pred mgr label e1) (compile_pred mgr label e2)
+      Bdd.disj mgr (compile_pred mgr ~label e1) (compile_pred mgr ~label e2)
 
 
-let rec compile_pol (mgr:Idd.manager) label expr = 
+let rec compile_pol (mgr:Idd.manager) ~label expr = 
   match expr with
   | Ext e -> failwith "Extensions not supported"
   | Filter b -> 
-      let mgr_dd = Idd.get_manager_dd mgr in
-      let bdd = compile_pred (Bdd.manager ~d_mgr:mgr_dd ()) label b in
-      Idd.idd_from_dd mgr_dd (bdd :> Dd.t)
+      let mgr_bdd = Idd.get_bdd_manager mgr in
+      let bdd = compile_pred mgr_bdd ~label b in
+      Idd.of_bdd bdd
   | Modify { name; value } -> Idd.set mgr (label name) value
   | Union (e1, e2) -> 
-      Idd.union mgr (compile_pol mgr label e1) (compile_pol mgr label e1)
+      Idd.union mgr (compile_pol mgr ~label e1) (compile_pol mgr ~label e2)
   | Seq (e1, e2) -> 
-      Idd.seq mgr (compile_pol mgr label e1) (compile_pol mgr label e1)
-  | Star e -> Idd.star mgr (compile_pol mgr label e)
-
+      Idd.seq mgr (compile_pol mgr ~label e1) (compile_pol mgr ~label e2)
+  | Star e -> Idd.star mgr (compile_pol mgr ~label e)
--- src/katb/compile.mli
+++ src/katb/compile.mli
@@ -1,6 +1,10 @@
 open Idd_
 open Ast
 
-val compile_pol : Idd.manager -> (string -> int) -> expr -> Idd.t
+(** [compile_pol mgr label expr] is the IDD representing [expr] where [label]
+    maps packet header names to their respective variable index *)
+val compile_pol : Idd.manager -> label:(string -> int) -> expr -> Idd.t
 
-val compile_pred : Bdd.manager -> (string -> int) -> bexpr -> Bdd.t
+(** [compile_pred mgr label expr] is the BDD representing [expr] where [label]
+    maps packet header names to their respective variable index *)
+val compile_pred : Bdd.manager -> label:(string -> int) -> bexpr -> Bdd.t
