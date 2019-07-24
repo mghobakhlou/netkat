2c11c6738809caf6260c5cabcc795992432eeeee
Steffen Smolka <smolka@cs.cornell.edu>
2019-07-24 18:23:54 -0400

    merge in Caleb's compiler

--- src/katb/idd_compiler.ml
+++ src/katb/idd_compiler.ml
@@ -1,8 +1,6 @@
 open Idds
-open Hom
+open Kat.Hom
 
-type b = Bdd.t
-type k = Idd.t
 
 (** [get_ba mgr] produces an instance of [Bdd.t ba] given a BDD manager [mgr] *)
 let get_ba mgr = {
@@ -13,28 +11,30 @@ let get_ba mgr = {
     neg = Bdd.neg mgr;
   }
 
-(** [interp_test_mgr mgr interp_fields test] is the BDD resulting from testing
-    the variable specificed by [interp_fields test.name] according to 
-    [test.value] *)
-let interp_test_mgr mgr interp_fields ({ name; value }:Packet_ast.test) = 
-  Bdd.test mgr (Var.inp (interp_fields name)) value
+let get_kat mgr = {
+    ba = get_ba (Idd.get_bdd_manager mgr);
+    assrt = Idd.of_bdd;
+    union = Idd.union mgr;
+    seq = Idd.seq mgr;
+    star = Idd.star mgr;
+  }
 
-let compile_bexp mgr ~interp_fields bexp = 
+(** [interp_test_mgr mgr interp_fields test] is the BDD resulting from testing
+    the variable specificed by [interp_fields test.name] according to
+    [test.value] *)
+let interp_test_mgr mgr interp_fields ({ var; value }: Ast.test) =
+  Bdd.test mgr (Var.inp (interp_fields var)) value
+
+let compile_bexp mgr ~interp_fields bexp =
   let ba = get_ba mgr in
   let interp_test = interp_test_mgr mgr interp_fields in
   interp_bexp ~ba ~interp_test bexp
 
 let compile_exp mgr ~interp_fields exp =
+  let kat = get_kat mgr in
   let bdd_mgr = Idd.get_bdd_manager mgr in
-  let ba = get_ba bdd_mgr in
-  let kat = {
-    ba = ba;
-    assrt = Idd.of_bdd;
-    union = Idd.union mgr;
-    seq = Idd.seq mgr;
-    star = Idd.star mgr;
-  } in
   let interp_test = interp_test_mgr bdd_mgr interp_fields in
-  let interp_act ({ name; value }:Packet_ast.act) = 
-    Idd.set mgr (interp_fields name) value in
+  let interp_act ({ var; value }: Ast.act) =
+    Idd.set mgr (interp_fields var) value
+  in
   interp_exp ~kat ~interp_test ~interp_act exp
