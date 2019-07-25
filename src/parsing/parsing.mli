00ed9645ed1dbd5eb19415dcf4dca65ec53b6c7b
Steffen Smolka <smolka@cs.cornell.edu>
2019-07-25 13:04:07 -0400

    Document parsing.mli

--- src/parsing/parsing.mli
+++ src/parsing/parsing.mli
@@ -1,8 +1,21 @@
+(** This module is a functor providing a saner interface to your parser. *)
+
 module type PARSER = sig
+  (** Tokens produced by lexer and consumed by parser.  
+      For menhir-generated parsers, [type token = Menhir_parser.token]. *)
   type token
+  
+  (** The parsing result type produced by your parser *)
   type result
+  
+  (** Exception thrown by lexer in case of errors. *)
   exception LexError of string
+  
+  (** Exception thrown by the parser.  
+      For menhir-generated parsers,
+      [exception ParseError = Menhir_parser.Error] *)
   exception ParseError
+  
   (* val pp_token : Format.formatter -> token -> unit *)
   val parse : (Lexing.lexbuf -> token) -> Lexing.lexbuf -> result
   val next_token : Lexing.lexbuf -> token
@@ -24,6 +37,8 @@ module type S = sig
       colorful error messages including the source location when errrors occur. *)
 end
 
+(** Pass your parser/lexer to this functor to obtain a nicer interface,
+    without having to write boilerplate code. *)
 module Make : functor (P:PARSER) -> S with
   type token = P.token and
   type result = P.result
