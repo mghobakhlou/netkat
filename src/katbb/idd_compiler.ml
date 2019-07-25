66ae27a0a9df6d1f6a073e7cc8b1abb57a2a68f3
Steffen Smolka <smolka@cs.cornell.edu>
2019-07-25 13:11:59 -0400

    document compiler

--- src/katbb/idd_compiler.ml
+++ src/katbb/idd_compiler.ml
@@ -1,7 +1,6 @@
 open Idds
 
-
-(** [bdd_ba mgr] produces an instance of [Bdd.t ba] given a BDD manager [mgr] *)
+(** BDDs form a Boolean algebra. *)
 let bdd_ba mgr : Bdd.t Kat.Hom.ba = {
     ctrue = Bdd.ctrue;
     cfalse = Bdd.cfalse;
@@ -10,6 +9,7 @@ let bdd_ba mgr : Bdd.t Kat.Hom.ba = {
     neg = Bdd.neg mgr;
   }
 
+(** IDDs form a KAT *)
 let idd_kat mgr : (Idd.t, Bdd.t) Kat.Hom.kat = {
     ba = bdd_ba (Idd.get_bdd_manager mgr);
     assrt = Idd.of_bdd;
@@ -19,11 +19,13 @@ let idd_kat mgr : (Idd.t, Bdd.t) Kat.Hom.kat = {
   }
 
 
+(* [bexp] compilation is a Boolean homomorphism from epressions to BDDs *)
 let compile_bexp ~mgr ~map_var bexp =
   Kat.Hom.map_bexp bexp ~ba:(bdd_ba mgr) ~map_test:(fun Ast.{var; value} ->
     Bdd.test mgr (Var.inp (map_var var)) value
   )
 
+(* [exp] compilation is a KAT homomorphism from epressions to IDDs *)
 let compile_exp ~mgr ~map_var exp =
   let bdd_mgr = Idd.get_bdd_manager mgr in
   Kat.Hom.map_exp exp 
