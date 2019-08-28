471a61505e701e79a7bbb824b5e6e41f38929b0e
cakoch10 <cakoch10@gmail.com>
2019-08-28 16:19:31 -0400

    fixed specs

--- src/katbv/ast.ml
+++ src/katbv/ast.ml
@@ -7,10 +7,10 @@ type test =
   (* [Test (v, z, n)] tests that [v_i=0] for all i such that [z_i=1] and that 
      [v_i=1] for all i such that [n_i=1] *)
   | Test of string * Bitstring.t * Bitstring.t
-  [@@deriving sexp]
+
 (* [act (v, z, n)] is a vector [v'] such that [v'_i=0] for all i such that 
    [z_i=1] and [v_i=1] for all i such that [n_i=1] *)
-type act = string * Bitstring.t * Bitstring.t [@@deriving sexp]
-
-type bexp = test Kat.Ast.bexp [@@deriving sexp]
-type exp = (act, test) Kat.Ast.exp [@@deriving sexp]
+and act = string * Bitstring.t * Bitstring.t
+and bexp = test Kat.Ast.bexp
+and exp = (act, test) Kat.Ast.exp 
+   [@@deriving sexp]
